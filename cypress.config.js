ae475995091f06bc34e90784456679090492d5e6
Thang LD <thang.ld@clv.local>
2024-06-07 16:49:03 +0700

    API Test

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,17 @@
+const cucumber = require('cypress-cucumber-preprocessor').default
 const { defineConfig } = require("cypress");
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('file:preprocessor', cucumber())
     },
     //specPattern: "./cypress/tests/*/*",
-    specPattern: "./cypress/tests2/**.*",
+    // specPattern: "./cypress/tests2/**.*",
+    //specPattern: "cypress/e2e/*.feature",
+    specPattern: 'cypress/**/*.{js,jsx,ts,tsx}',
 
     baseUrl: "https://chorus-dev.one-line.com/bkm/booking",
     
---
+++ cypress/e2e/newtours/newtours.js
@@ -0,0 +1,12 @@
+import{Given, When, Then} from "cypress-cucumber-preprocessor/steps";
+
+Given("precondition", ()=>{
+    cy.visit("https://demo.guru99.com/test/newtours/")
+})
+
+When("action", ()=>{
+    cy.title().should('eq','Welcome: Mercury Tours')
+})
+Then("testable outcome",()=>{
+    cy.title().should('eq','Welcome: Mercury Tours')
+})
